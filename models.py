1d756b58332f5b0086e0f0c9626f5506aff5f970
Amrit <anandamrit309@gmail.comm>
2025-07-05 17:36:00 +0530

    added autoincrement in model

--- models.py
+++ models.py
@@ -4,6 +4,6 @@ from database import Base
 class Medicine(Base):
     __tablename__ = "medicines"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String, index=True)
     price = Column(Integer)
